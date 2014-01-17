8e54746c0f918b58b1373111731ed4b3ac243507
Andrey Koleshko <ka8725@gmail.com>
2014-01-17 15:30:43 +0300

    Implement authorize stub

--- server.rb
+++ server.rb
@@ -1,6 +1,25 @@
 require 'sinatra'
 require 'json'
 
+API = '/api/v1'.freeze
+
+def json(path)
+  path_with_req_method = path.gsub(API, "#{API}/#{request_method}")
+  ruby_file = ".#{path_with_req_method}.rb"
+
+  return "#{ruby_file} is absent" unless File.exists?(ruby_file)
+
+  eval(File.read(ruby_file)).to_json
+end
+
+def request_method
+  @request_method ||= env['REQUEST_METHOD'].downcase
+end
+
 get '*' do |path|
-  eval(File.read(".#{path}.rb")).to_json
+  json(path)
+end
+
+post '*' do |path|
+  json(path)
 end
